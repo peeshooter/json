ba76f0e5ae1b9d1d81db5d15f50b94aa87fedf4d
420385517@qq.com <420385517@qq.com>
2021-03-02 15:14:00 +0800

    bbb

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e939b5d700330e76f5d151ecadf7b21861bfdd20',
+    'name' => 'phoenix/json',
+  ),
+  'versions' => 
+  array (
+    'phoenix/json' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e939b5d700330e76f5d151ecadf7b21861bfdd20',
+    ),
+  ),
+);
